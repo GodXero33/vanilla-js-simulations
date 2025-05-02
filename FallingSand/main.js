8f510c1f03fd9da970d233018dffa0d01c51ac48
GodXero <godxero.dev@gmail.com>
2025-05-02 23:09:05 +0530

    initialze 'faling sand sim'

--- FallingSand/main.js
+++ FallingSand/main.js
@@ -3,48 +3,9 @@ import * as AnimatorModule from '../common/js/common.main.js';
 AnimatorModule.setControllerProperty('commonWarnings', true);
 AnimatorModule.setControllerProperty('autoPlay', true);
 
-// <<< ===================== REMOVE
-
-class Dummy {
-	constructor (x, y, r, v, color) {
-		this.x = x;
-		this.y = y;
-		this.r = r;
-		this.v = v;
-		this.t = 0;
-		this.color = color;
-	}
-
-	update () {
-		this.t += this.v;
-		this.x = this.r * Math.cos(this.t);
-		this.y = this.r * Math.sin(this.t);
-	}
-
-	draw (ctx) {
-		ctx.fillStyle = this.color;
-
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
-		ctx.fill();
-	}
-}
-
-const dummies = [];
-
-for (let a = 0; a < 10; a++) {
-	dummies.push(new Dummy(0, 0, 30 + a * 25, 0.01, '#f00'));
-}
-
-// REMOVE ===================== >>>
-
 const canvas = AnimatorModule.createCanvas({
 	parent: document.getElementById('main-container'),
-	update: () => {
-		// <<< ===================== REMOVE
-		dummies.forEach(dummy => dummy.update());
-		// REMOVE ===================== >>>
-	},
+	update: () => {},
 	draw: (ctx) => {
 		ctx.fillStyle = '#121212';
 		
@@ -54,17 +15,8 @@ const canvas = AnimatorModule.createCanvas({
 		
 		ctx.translate(canvas.width * 0.5, canvas.height * 0.5);
 
-		// <<< ===================== REMOVE
-		dummies.forEach(dummy => dummy.draw(ctx));
-		// REMOVE ===================== >>>
-
 		ctx.setTransform(transform);
-	},
-	// <<< ===================== REMOVE
-	resize: (ctx) => {
-		console.log(ctx, canvas.width, canvas.height);
 	}
-	// REMOVE ===================== >>>
 });
 
 console.log(canvas);
