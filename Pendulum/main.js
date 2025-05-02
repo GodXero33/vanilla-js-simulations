2c9c00d981a2efd8f0c6704ac437ed5b2f6ad71b
GodXero <godxero.dev@gmail.com>
2025-05-02 12:13:46 +0530

    Pendulam motion done. dat.gui.module.js added

--- Pendulum/main.js
+++ Pendulum/main.js
@@ -1,21 +1,69 @@
 import * as AnimatorModule from '../common/js/common.main.js';
+import Pendulum from './pendulum.js';
+import * as dat from '../common/js/lib/dat.gui.module.js';
 
 AnimatorModule.setControllerProperty('commonWarnings', true);
 AnimatorModule.setControllerProperty('autoPlay', true);
 
+const pendulum = new Pendulum(300);
+const gui = new dat.GUI();
+
+const pendulumFolder = gui.addFolder('Pendulum Controls');
+
+pendulumFolder.add(pendulum, 'length', 100, 1000).onChange(val => {
+	pendulum.length = val;
+
+	pendulum.update();
+});
+
+pendulumFolder.add(pendulum, 'gravity', 0.1, 20).step(0.1);
+pendulumFolder.add(pendulum, 'frictionFact', 0, 0.1).step(0.001);
+pendulumFolder.add(pendulum, 'mass', 0, 100).step(1);
+pendulumFolder.add(pendulum, 'drawHistory');
+pendulumFolder.add({ reset: () => pendulum.reset() }, 'reset');
+pendulumFolder.add({ pause: () => { canvas.isPlaying ? canvas.pause() : canvas.play(); } }, 'pause').name('play / pause');
+
+pendulumFolder.open();
+
+function drawEnergy (ctx, label, energy, y) {
+	ctx.font = '18px Arial';
+	ctx.textBaseline = 'middle';
+
+	const energyStr =  `${label}: ${energy.toFixed(3)} J`;
+	const textMeasure = ctx.measureText(energyStr);
+	const padding = 10;
+	const textWidth = textMeasure.width + padding * 2;
+	const textHeight = textMeasure.actualBoundingBoxAscent + textMeasure.actualBoundingBoxDescent + padding * 2;
+
+	ctx.fillStyle = '#232323';
+	ctx.fillRect(0, textHeight * y, textWidth, textHeight);
+
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(energyStr, padding, textHeight * (y + 0.5));
+}
+
 const canvas = AnimatorModule.createCanvas({
 	parent: document.getElementById('main-container'),
-	update: () => {},
+	update: () => {
+		pendulum.update();
+	},
 	draw: (ctx) => {
 		ctx.fillStyle = '#121212';
-		
+
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 		const transform = ctx.getTransform();
-		
-		ctx.translate(canvas.width * 0.5, canvas.height * 0.5);
 
+		ctx.translate(canvas.width * 0.5, 0);
+		pendulum.draw(ctx);
 		ctx.setTransform(transform);
+
+		const energies = pendulum.getEnergies();
+
+		drawEnergy(ctx, 'Kinetic Energy', energies.kinetic, 0);
+		drawEnergy(ctx, 'Potential Energy', energies.potential, 1);
+		drawEnergy(ctx, 'Total Energy', energies.total, 2);
+		drawEnergy(ctx, 'Energy lost', energies.lost, 3);
 	}
 });
 
--- Pendulum/pendulum.js
+++ Pendulum/pendulum.js
@@ -1,5 +1,100 @@
-class Pendulum {
+export default class Pendulum {
 	constructor (length) {
 		this.length = length;
+		this.angleV = 0;
+		this.angle = 0;
+		this.pinPoint = [0, 0];
+		this.bob = [0, this.length];
+		this.gravity = 2;
+		this.frictionFact = 0.005;
+		this.mass = 1;
+		this.history = [];
+		this.historyLength = 50;
+		this.drawHistory = false;
+		this.lastEnergy = 0;
+	}
+
+	update () {
+		const angleA = Math.cos(this.angle) * this.gravity / this.length;
+
+		this.angleV += angleA;
+		this.angle += this.angleV;
+		this.angleV *= 1 - this.frictionFact;
+
+		this.bob[0] = Math.cos(this.angle) * this.length + this.pinPoint[0];
+		this.bob[1] = Math.sin(this.angle) * this.length + this.pinPoint[1];
+
+		this.history.push(...this.bob);
+
+		if (this.history.length > this.historyLength * 2) {
+			this.history.shift();
+			this.history.shift();
+		}
+	}
+
+	drawPath (ctx) {
+		ctx.strokeStyle = '#ffff00';
+		ctx.lineCap = 'round';
+		ctx.lineWidth = 5;
+
+		ctx.beginPath();
+
+		ctx.moveTo(this.history[0], this.history[1]);
+		ctx.lineTo(this.history[0], this.history[1]);
+
+		for (let a = 2; a < this.history.length; a += 2) {
+			ctx.moveTo(this.history[a], this.history[a + 1]);
+			ctx.lineTo(this.history[a], this.history[a + 1]);
+		}
+
+		ctx.stroke();
+	}
+
+	draw (ctx) {
+		if (this.drawHistory && this.history.length > 3) this.drawPath(ctx);
+
+		ctx.fillStyle = '#ffffff';
+		ctx.strokeStyle = '#787890';
+		ctx.lineWidth = 2;
+
+		ctx.beginPath();
+		ctx.moveTo(this.pinPoint[0], this.pinPoint[1]);
+		ctx.lineTo(this.bob[0], this.bob[1]);
+		ctx.stroke();
+
+		ctx.beginPath();
+		ctx.arc(this.pinPoint[0], this.pinPoint[1], 10, 0, Math.PI * 2, false);
+		ctx.fill();
+
+		ctx.beginPath();
+		ctx.arc(this.bob[0], this.bob[1], 30, 0, Math.PI * 2, false);
+		ctx.fill();
+	}
+
+	reset () {
+		this.angle = 0;
+		this.angleV = 0;
+
+		this.update();
+	}
+
+	getEnergies () {
+		const m = this.mass;
+		const g = this.gravity;
+		const L = this.length;
+		const theta = this.angle;
+		const omega = this.angleV;
+
+		const h = L * (1 - Math.sin(theta));
+
+		const potential = m * g * h;
+		const velocity = omega * L;
+		const kinetic = 0.5 * m * velocity * velocity;
+		const total = potential + kinetic;
+		const lost = this.lastEnergy - total;
+
+		this.lastEnergy = total;
+
+		return { potential, kinetic, total, lost };
 	}
 }
